b96d90a025a095e830eb6f437bcfeb2fc2faacdb
zeyd01 <zeydiyigoren@gmail.com>
2023-12-12 18:58:26 +0300

    register page is being built

--- app/src/main/java/com/example/yemekai/MainActivity.java
+++ app/src/main/java/com/example/yemekai/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.yemekai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button register,storage,searchResult,login,favoriteFood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register=findViewById(R.id.button2);
+        register.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, register.class);
+            startActivity(intent);
+        });
+        storage=findViewById(R.id.button3);
+        storage.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, storage.class);
+            startActivity(intent);
+        });
+        searchResult=findViewById(R.id.button4);
+        searchResult.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, searchResult.class);
+            startActivity(intent);
+        });
+        login=findViewById(R.id.button5);
+        login.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+        });
+        favoriteFood=findViewById(R.id.button6);
+        favoriteFood.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, favFood.class);
+            startActivity(intent);
+        });
     }
+
 }
--- app/src/main/java/com/example/yemekai/register.java
+++ app/src/main/java/com/example/yemekai/register.java
@@ -1,14 +1,69 @@
 package com.example.yemekai;
 
+import static android.text.TextUtils.isEmpty;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
+
+    EditText name,email,password,age;
+    CheckBox rule1;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        name=findViewById(R.id.editTextText);
+        email=findViewById(R.id.editTextTextEmailAddress);
+        password=findViewById(R.id.editTextTextPassword);
+        age=findViewById(R.id.editTextNumber);
+        rule1=findViewById(R.id.checkBox);
+        register=findViewById(R.id.button);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+            }
+        });
+
     }
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+void checkDataEntered(){
+    if (isEmpty(name)) {
+        name.setError("Name is required");
+    }
+    if (isEmail(email)==false) {
+        email.setError("Enter a valid E-Mail");
+    }
+    if (isEmpty(password)) {
+        password.setError("Password is required");
+    }
+    if (isEmpty(age)) {
+        age.setError("Age is required");
+    }
+    if(!rule1.isChecked()){
+        Toast t = Toast.makeText(this, "You must agree on the rule to register!", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+}
 }
