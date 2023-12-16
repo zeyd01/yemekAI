a26a06e415a52e8b118c3f1a3cb60cd8eb74c04e
zeyd01 <zeydiyigoren@gmail.com>
2023-12-16 11:10:45 +0300

    register is done

--- app/src/main/java/com/example/yemekai/register.java
+++ app/src/main/java/com/example/yemekai/register.java
@@ -4,6 +4,7 @@ import static android.text.TextUtils.isEmpty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -14,26 +15,30 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class register extends AppCompatActivity {
 
-    EditText name,email,password,age;
+    EditText name,email1,password1,age;
     CheckBox rule1;
     Button register;
-
+    private List<Account> accounts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         name=findViewById(R.id.editTextText);
-        email=findViewById(R.id.editTextTextEmailAddress);
-        password=findViewById(R.id.editTextTextPassword);
+        email1=findViewById(R.id.editTextTextEmailAddress);
+        password1=findViewById(R.id.editTextTextPassword);
         age=findViewById(R.id.editTextNumber);
         rule1=findViewById(R.id.checkBox);
         register=findViewById(R.id.button);
+        accounts = new ArrayList<>();
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkDataEntered();
+                createButton();
             }
         });
 
@@ -47,23 +52,67 @@ public class register extends AppCompatActivity {
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
-void checkDataEntered(){
-    if (isEmpty(name)) {
+public boolean checkDataEntered(){
+
+   if (isEmpty(name)) {
         name.setError("Name is required");
+        return false;
     }
-    if (isEmail(email)==false) {
-        email.setError("Enter a valid E-Mail");
+    if (isEmail(email1)==false) {
+        email1.setError("Enter a valid E-Mail");
+        return false;
     }
-    if (isEmpty(password)) {
-        password.setError("Password is required");
+    if (isEmpty(password1)) {
+        password1.setError("Password is required");
+        return false;
     }
     if (isEmpty(age)) {
         age.setError("Age is required");
+        return false;
     }
     if(!rule1.isChecked()){
         Toast t = Toast.makeText(this, "You must agree on the rule to register!", Toast.LENGTH_SHORT);
         t.show();
+        return false;
+    }
+    else return true;
+}
+void createButton(){
+        checkDataEntered();
+        if(checkDataEntered()==true){
+            createAccount();
+            Intent intent = new Intent(register.this, MainActivity.class);
+            startActivity(intent);
+            Toast t = Toast.makeText(this, "You successfully created an account", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+    }
+    private class Account {
+        private String username;
+        private String password;
+        private String email;
+
+        public Account(String username, String password, String email) {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+        }
     }
 
-}
+    private void createAccount() {
+        String username = name.getText().toString();
+        String password = password1.getText().toString();
+        String email = email1.getText().toString();
+
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+
+            return;
+        }
+        Account account = new Account(username, password, email);
+        accounts.add(account);
+
+    }
+
+
 }
