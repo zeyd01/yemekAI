10af30f7d1cf5262f92d03e7ddc021f3cc08a126
zeyd01 <zeydiyigoren@gmail.com>
2023-12-16 12:04:33 +0300

    massive bug fixed, Login screen created

--- app/src/main/java/com/example/yemekai/login.java
+++ app/src/main/java/com/example/yemekai/login.java
@@ -3,12 +3,22 @@ package com.example.yemekai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
+    TextView tw1;
+    EditText e1,e2;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        e1=findViewById(R.id.editTextTextEmailAddress3);
+        e2=findViewById(R.id.editTextTextPassword3);
+        b1=findViewById(R.id.button8);
+        tw1=findViewById(R.id.textView2);
     }
 }
